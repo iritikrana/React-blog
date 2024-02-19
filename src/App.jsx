5f3efb6253e75202e7f6db859a06cea5a854a30b
Ranjan Kumar <ranjanj@edgecorporation.net>
2024-02-19 19:40:41 +0530

    redux tailwind components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1>a blog with appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((err) =>
+        console.log(
+          `error occured while getting current user details => ${err}`
+        )
+      )
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -57,4 +57,6 @@ export class AuthService {
   }
 }
 
-export default AuthService;
+
+const authService = new AuthService();
+export default authService;
